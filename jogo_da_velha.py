a8a1154cc611447c2f3cebf374b796ea1e1eec7f
Pedro Peixoto Viana de Oliveira <85454019+pedro-peixoto-23@users.noreply.github.com>
2022-10-06 15:37:39 -0300

    jogo_da_velha

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -9,43 +9,49 @@ jogadas = 0
 lista_coordenadas = []
 
 
+def limpar_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
 def pedir_usuario_jogadas():
     global jogadas
     global jogador
     global lista_coordenadas
-    
-    os.system('cls' if os.name == 'nt' else 'clear')
 
     if jogadas % 2 == 0:
-        jogador = 1
+        jogador = "X"
     else:
-        jogador = 2
+        jogador = "O"
     
     imprimir_jogo_velha()
 
     print(f"\nJogadas: {jogadas}")
-    print(f"Jogador: {jogador}")
+    print(f"Jogador atual: {jogador}")
 
     try:
         linha_usuario = int(input("\nInsira a linha: "))
         coluna_usuario = int(input("Insira a coluna: "))
 
+        limpar_terminal()
+
         coordenada = (linha_usuario, coluna_usuario)
 
         if linha_usuario > 3 or linha_usuario < 1 or coluna_usuario > 3 or coluna_usuario < 1:
-            os.system('cls' if os.name == 'nt' else 'clear')
+            limpar_terminal()
             print("Insira apenas números entre 1 e 3!")
             pedir_usuario_jogadas()
+            
         if coordenada in lista_coordenadas:
+            limpar_terminal()
             print(f"\nPosição {coordenada} já foi ocupada!")
             pedir_usuario_jogadas()
         else:
             lista_coordenadas.append(coordenada)
     except:
-        os.system('cls' if os.name == 'nt' else 'clear')
+        limpar_terminal()
         print("Insira apenas números entre 1 e 3!")
         pedir_usuario_jogadas()
-
+    
     jogadas += 1
 
     atualizar_matriz(linha_usuario, coluna_usuario, jogador)
@@ -54,10 +60,7 @@ def pedir_usuario_jogadas():
 def atualizar_matriz(linha, coluna, jogador):
     global matriz
 
-    if jogador == 1:
-        X_ou_O = "X"
-    else:
-        X_ou_O = "O"
+    X_ou_O = jogador
     
     if linha == 1 and coluna == 1:
         matriz[0][0] = X_ou_O
@@ -86,55 +89,135 @@ def atualizar_matriz(linha, coluna, jogador):
 
 def imprimir_jogo_velha():
     global matriz
-        
-    print("--1---2---3")
-    print(f"1 {matriz[0][0]} | {matriz[1][0]} | {matriz[2][0]}")
-    print("| ---------")
-    print(f"2 {matriz[3][0]} | {matriz[4][0]} | {matriz[5][0]}")
-    print("| ---------")
-    print(f"3 {matriz[6][0]} | {matriz[7][0]} | {matriz[8][0]}")
+
+    print("    1   2   3")
+    print("  ┌───┬───┬───┐")
+    print(f"1 │ {matriz[0][0]} │ {matriz[1][0]} │ {matriz[2][0]} │ 1")
+    print("  ├───┼───┼───┤")
+    print(f"2 │ {matriz[3][0]} │ {matriz[4][0]} │ {matriz[5][0]} │ 2")
+    print("  ├───┼───┼───┤")
+    print(f"3 │ {matriz[6][0]} │ {matriz[7][0]} │ {matriz[8][0]} │ 3")
+    print("  └───┴───┴───┘")
+    print("    1   2   3")
 
 
 def verificar_vitoria_empate():
     global jogador
     global matriz
 
-    if jogador == 1:
-        X_ou_O = "O"
-    else:
-        X_ou_O = "X"
+    X_ou_O = jogador
     
     #verificar vitória
     if  matriz[0][0] == matriz[4][0] == matriz[8][0] and " " not in (matriz[0][0], matriz[4][0], matriz[8][0]):
-        print(f"\nVencedor é o jogador {jogador}")
+        imprimir_vencedor(1)
         return 0 # vencedor
     elif matriz[6][0] == matriz[4][0] == matriz[2][0] and " " not in (matriz[6][0], matriz[4][0], matriz[2][0]):
-        print(f"\nVencedor é o jogador {jogador}")
-        return 0 
+        imprimir_vencedor(2)
+        return 0
     #vitoria nas linhas
     elif matriz[0][0] == matriz[1][0] == matriz[2][0] and " " not in (matriz[0][0], matriz[1][0], matriz[2][0]):
-        print(f"\nVencedor é o jogador {jogador}")
+        imprimir_vencedor(3)
         return 0
     elif matriz[3][0] == matriz[4][0] == matriz[5][0] and " " not in (matriz[3][0], matriz[4][0], matriz[5][0]):
-        print(f"\nVencedor é o jogador {jogador}")
+        imprimir_vencedor(4)
         return 0
     elif matriz[6][0] == matriz[7][0] == matriz[8][0] and " " not in (matriz[6][0], matriz[7][0], matriz[8][0]):
-        print(f"\nVencedor é o jogador {jogador}")
+        imprimir_vencedor(5)
         return 0
     #vitoria nas colunas
     elif matriz[0][0] == matriz[3][0] == matriz[6][0] and " " not in (matriz[0][0], matriz[3][0], matriz[6][0]):
-        print(f"\nVencedor é o jogador {jogador}")
+        imprimir_vencedor(6)
         return 0
     elif matriz[1][0] == matriz[4][0] == matriz[7][0] and " " not in (matriz[1][0], matriz[4][0], matriz[7][0]):
-        print(f"\nVencedor é o jogador {jogador}")
+        imprimir_vencedor(7)
         return 0
     elif matriz[2][0] == matriz[5][0] == matriz[8][0] and " " not in (matriz[2][0], matriz[5][0], matriz[8][0]):
-        print(f"\nVencedor é o jogador {jogador}")
+        imprimir_vencedor(8)
         return 0
     #verificar empate
     elif " " not in (matriz[0][0], matriz[1][0], matriz[2][0], matriz[3][0], matriz[4][0], matriz[5][0], matriz[6][0], matriz[7][0], matriz[8][0]):
-        print(f"\nOcorreu um empate!")
+        imprimir_jogo_velha()
+        print(f"\nDeu velha!")
         return 0
 
 
+def imprimir_vencedor(x):
+    global jogador
+
+    imprimir_resultado_formatado(x)
+    print(f"\nVencedor é o jogador {jogador}")
+    reiniciar_game()
+
+
+def imprimir_resultado_formatado(x):
+    global matriz
+    global jogador
+
+    jogador_formatado = "\033[0;32m{}\033[0;0m".format(jogador)
+
+    if x == 1:
+        matriz[0][0] = jogador_formatado
+        matriz[4][0] = jogador_formatado
+        matriz[8][0] = jogador_formatado
+    elif x == 2:
+        matriz[6][0] = jogador_formatado
+        matriz[4][0] = jogador_formatado
+        matriz[2][0] = jogador_formatado
+    elif x == 3:
+        matriz[0][0] = jogador_formatado
+        matriz[1][0] = jogador_formatado
+        matriz[2][0] = jogador_formatado
+    elif x == 4:
+        matriz[3][0] = jogador_formatado
+        matriz[4][0] = jogador_formatado
+        matriz[5][0] = jogador_formatado
+    elif x == 5:
+        matriz[6][0] = jogador_formatado
+        matriz[7][0] = jogador_formatado
+        matriz[8][0] = jogador_formatado
+    elif x == 6:
+        matriz[0][0] = jogador_formatado
+        matriz[3][0] = jogador_formatado
+        matriz[6][0] = jogador_formatado
+    elif x == 7:
+        matriz[1][0] = jogador_formatado
+        matriz[4][0] = jogador_formatado
+        matriz[7][0] = jogador_formatado
+    elif x == 8:
+        matriz[2][0] = jogador_formatado
+        matriz[5][0] = jogador_formatado
+        matriz[8][0] = jogador_formatado
+
+    imprimir_jogo_velha()
+
+
+def reiniciar_game():
+    global matriz
+    global jogadas
+    global lista_coordenadas
+
+    while True:
+        try:
+            resposta = input("\nVocê deseja jogar novamente? (s/n): ")
+
+            if resposta == "s":
+
+                matriz = [[" "], [" "], [" "],
+                          [" "], [" "], [" "],
+                          [" "], [" "], [" "]]
+                jogadas = 0
+                lista_coordenadas = []
+
+                limpar_terminal()
+                pedir_usuario_jogadas()
+            else:
+                limpar_terminal()
+                print("Muito obrigado por usar o programa!")
+
+        except:
+            continue
+
+
+limpar_terminal()
 pedir_usuario_jogadas()
+
